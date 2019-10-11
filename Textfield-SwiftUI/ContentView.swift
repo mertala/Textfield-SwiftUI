49840cb75cac3e816cd2a678c5651f4d9f832e89
Mert <mertala@icloud.com>
2019-10-11 16:24:18 +0300

    Update ContentView.swift

--- Textfield-SwiftUI/ContentView.swift
+++ Textfield-SwiftUI/ContentView.swift
@@ -7,15 +7,49 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    @State var email: String = ""
+    @State var sifre: String = ""
+    
+   
+    var body: some View
+   {
+        
+    VStack {
+       
+        TextField("Email gir", text: $email)
+           
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+            .frame(width: 300, height: 60)
+            .shadow(radius: 5.0)
+            .keyboardType(UIKeyboardType.emailAddress)
+        
+        SecureField("Şifre gir", text: $sifre)
+            
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 300, height: 60)
+            .padding(.bottom, 10)
+            .shadow(radius: 5.0)
+            .keyboardType(UIKeyboardType.numberPad)
+         
+          
+        Button(action: {}) {
+        
+            HStack {
+            
+            Text("Giriş")}
+      
+         .frame(minWidth: 0, maxWidth: .infinity)
+         .padding()
+         .foregroundColor(.white)
+         .background(Color.green)
+         .frame(width: 300, height: 60)
+         .cornerRadius(30.0)
+         .shadow(radius: 5.0)
+            }
+        }
     }
 }
